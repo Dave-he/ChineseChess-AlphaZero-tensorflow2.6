f8d18079f79609607cf885648a0cc780afecb426
heyongxian <1064042411@qq.com>
2021-10-18 22:59:13 +0800

    tensorflow-2.6

---
+++ cchess_alphazero/lib/tf_util.py
@@ -0,0 +1,21 @@
+
+def set_session_config(per_process_gpu_memory_fraction=None, allow_growth=None, device_list='0'):
+    """
+
+    :param allow_growth: When necessary, reserve memory
+    :param float per_process_gpu_memory_fraction: specify GPU memory usage as 0 to 1
+
+    :return:
+    """
+    import tensorflow as tf
+    import keras.backend as K
+
+    config = tf.compat.v1.ConfigProto(
+        gpu_options=tf.compat.v1.GPUOptions(
+            per_process_gpu_memory_fraction=per_process_gpu_memory_fraction,
+            allow_growth=allow_growth,
+            visible_device_list=device_list
+        )
+    )
+    sess = tf.compat.v1.Session(config=config)
+    K.set_session(sess)
